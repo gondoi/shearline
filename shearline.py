9ffb0df2a793999ac69a7c538076cdbeaff157b4
Trey Tabner <trey.tabner@rackspace.com>
2012-07-27 23:43:14 -0500

    Messing around with multiprocessing

--- shearline.py
+++ shearline.py
@@ -1,43 +1,46 @@
+#!/usr/bin/env python
+
+from multiprocessing import Pool
+
 import cloudfiles
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-import os, time
+import signal, os, sys, time
 
-while True:
-    try:
-        s3 = S3Connection(is_secure=True, anon=True)
-        bucket = s3.get_bucket(os.environ['S3_BUCKET'])
 
-        cf = cloudfiles.get_connection(os.environ['CF_USERNAME'], os.environ['CF_APIKEY'])
-        container = cf.create_container(os.environ['CF_CONTAINER'])
+def sync(key):
+    s3 = S3Connection(is_secure=True, anon=True)
+    bucket = s3.get_bucket(os.environ['S3_BUCKET'])
+    item = bucket.get_key(key)
+    cf = cloudfiles.get_connection(os.environ['CF_USERNAME'],
+                                   os.environ['CF_APIKEY'])
+    container = cf.create_container(os.environ['CF_CONTAINER'])
+    cf_object = container.create_object(key)
 
-        for item in bucket.list():
-            cf_object = container.create_object(item.key)
+    if item.size > 0:
+        if cf_object.etag is None or item.etag != '"%s"' % cf_object.etag:
+            print ("Creating %s on Cloud Files" % item.key)
+            cf_object.send(item)
+        else:
+            print ("%s Already exists and is up-to-date" % item.key)
 
-            print '---------------------------------------------------------------------'
-            print item.key
-            print '---------------------------------------------------------------------'
-            if item.size > 0:
-                print 'CF Etag: "%s"' % cf_object.etag
-                print 'S3 Etag: %s' % item.etag
+    else:
+        print("Skipping %s of size %s" % (item.key, item.size))
 
-                if cf_object.etag is None or item.etag != '"%s"' % cf_object.etag:
-                    print ("Creating %s on Cloud Files" % item.key)
-                    cf_object.send(item)
-                    print ("File creation complete")
-                else:
-                    print ("%s Already exists and is up-to-date" % item.key)
 
-            else:
-                print("Skipping %s of size %s" % (item.key, item.size))
+def main():
+    s3 = S3Connection(is_secure=True, anon=True)
+    bucket = s3.get_bucket(os.environ['S3_BUCKET'])
 
-            print "====================================================================="
-            print ""
+    keys = []
+    for item in bucket.list():
+        keys.append(item.key)
 
-        break
+    p = Pool(processes=4)
+    p.map(sync, keys)
 
-    except Exception, e:
-        print "ERROR: %s" % e
-        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
