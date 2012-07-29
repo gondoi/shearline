acebf6f18cdbd2af32f207f80f86c1a29efe2b52
Trey Tabner <trey.tabner@rackspace.com>
2012-07-28 20:09:00 -0500

    Getting started with Python multiprocessing

--- shearline.py
+++ shearline.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-from multiprocessing import Pool
+import multiprocessing
+import os
+import signal
+import time
+import Queue
 
 import cloudfiles
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-import signal, os, sys, time
+def synchronize(key):
+    print 'Started: %s' % key
 
-
-def sync(key):
     s3 = S3Connection(is_secure=True, anon=True)
     bucket = s3.get_bucket(os.environ['S3_BUCKET'])
     item = bucket.get_key(key)
@@ -21,26 +24,57 @@ def sync(key):
 
     if item.size > 0:
         if cf_object.etag is None or item.etag != '"%s"' % cf_object.etag:
-            print ("Creating %s on Cloud Files" % item.key)
             cf_object.send(item)
+            status = "Created on Cloud Files: %s" % item.key
         else:
-            print ("%s Already exists and is up-to-date" % item.key)
-
+            status = "Already exists and is up-to-date: %s" % item.key
     else:
-        print("Skipping %s of size %s" % (item.key, item.size))
+        status = "Skipping empty item: %s" %item.key
 
+    print status
+    return status
+
+def process(total, job_queue, result_queue):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    while not job_queue.empty():
+        try:
+            progress = 100 - float(job_queue.qsize()) / total * 100
+            print "Progress: %.2f%%" % progress
+
+            job = job_queue.get(block=False)
+            result_queue.put(synchronize(job))
+        except Queue.Empty:
+            pass
 
 def main():
+    job_queue = multiprocessing.Queue()
+    result_queue = multiprocessing.Queue()
+
     s3 = S3Connection(is_secure=True, anon=True)
     bucket = s3.get_bucket(os.environ['S3_BUCKET'])
 
-    keys = []
     for item in bucket.list():
-        keys.append(item.key)
+        job_queue.put(item.key)
+    total = job_queue.qsize()
 
-    p = Pool(processes=4)
-    p.map(sync, keys)
+    workers = []
+    for i in range(1):
+        tmp = multiprocessing.Process(target=process,
+                                      args=(total, job_queue, result_queue))
+        tmp.start()
+        workers.append(tmp)
 
+    try:
+        for worker in workers:
+            worker.join()
+    except KeyboardInterrupt:
+        print 'parent received ctrl-c'
+        for worker in workers:
+            worker.terminate()
+            worker.join()
 
-if __name__ == '__main__':
+    while not result_queue.empty():
+        print result_queue.get(block=False)
+
+if __name__ == "__main__":
     main()
