b4b13f9f872bbcd60064844996f6a316a361cc6c
Trey Tabner <trey.tabner@rackspace.com>
2012-07-28 21:38:39 -0500

    Update setup.py packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ python_libraries = ['boto', 'python-cloudfiles']
 
 setup(
     name='shearline',
-    packages=['.'],
+    packages=[],
     entry_points={
         'console_scripts': [
             'shearline = shearline:main',
